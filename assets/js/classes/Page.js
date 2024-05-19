7f490bac9e7540c7f911331c82cb93f5c777dedf
mashoun <mashoun.me@gmail.com>
2024-05-19 14:55:51 +0300

    admin is seperated from the site

--- assets/js/classes/Page.js
+++ assets/js/classes/Page.js
@@ -6,6 +6,7 @@ export default class Page {
             'status',
             'folder',
             'media',
+            'mediaLinks',
             'title',
             'subtitle',
             'badge',
