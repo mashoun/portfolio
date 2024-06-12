9bd2a6eccd785203734c26c9c6c54c3a64484bda
mashoun <mashoun.me@gmail.com>
2024-06-12 22:41:48 +0300

    *Page *styles +comingsoon btns *logo is linked and skeleton

--- assets/js/components/page/index.js
+++ assets/js/components/page/index.js
@@ -1,6 +1,7 @@
 import utilities from "../../utilities.js"
 import store from '../../store.js'
 import Page from '../../classes/Page.js'
+import Allo from '../../classes/Allo.js'
 export default {
     template: await utilities.getPage('/assets/js/components/page/index.html'),
     data() {
@@ -24,6 +25,9 @@ export default {
             var article = document.querySelector('article')
             // article.style.backgroundColor = `#0d1b2a`
             article.style.filter = this.darkMode ? 'invert(1)' : 'invert(0)'
+        },
+        comingsoon(){
+            new Allo('Thanks for your feedback!',"Likes, comments & translations are coming soon! Share this page to show your love in the meantime!").run()
         }
     }
 }
